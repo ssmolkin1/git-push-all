1f77506d83f675fba63458db2304f35c78b1c2b0
Sam <sam@future-precedent.org>
2017-10-25 01:33:21 +0100

    first real gal commit

--- gal.js
+++ gal.js
@@ -8,16 +8,6 @@ const commandLineCommands = require('command-line-commands');
 const configPath = './config.json';
 const config = require(configPath);
 
-const currentBranch = shell.exec('git branch | grep \\*', {silent:true}).stdout.slice(2);
-
-shell.exec(`git add -A &&
-  git commit -m "gal-commit" &&
-  git push -u ${config.remote} ${currentBranch}`);
-
-/* fs.writeFileSync(configPath, JSON.stringify(config, null, 2));
-
-console.log('Complete!');
-
 const validCommands = [null, 'config'];
 const {command, argv} = commandLineCommands(validCommands);
 
@@ -26,7 +16,34 @@ const {command, argv} = commandLineCommands(validCommands);
 if (command === null) {
   const optionDefinitions = [
     {
-      name: ''
+      name: 'message',
+      alias: 'm',
+      type: String
+    },
+    {
+      name: 'remote',
+      alias: 'r',
+      type: String
+    },
+    {
+      name: 'branch',
+      alias: 'b',
+      type: String
     }
-  ]
-} */
+  ];
+  
+  const options = commandLineArgs(optionDefinitions, {argv});
+
+  const currentBranch = shell.exec('git branch | grep \\*', {silent:true}).stdout.slice(2);
+
+  function gal(message = config.message, remote = config.remote, branch = currentBranch) {
+    shell.exec(`git add -A &&
+      git commit -m "${message}" &&
+      git push -u ${remote} ${branch}`);
+  }
+  
+  gal(options.message, options.remote, options.branch);
+}
+  
+
+// fs.writeFileSync(configPath, JSON.stringify(config, null, 2));
