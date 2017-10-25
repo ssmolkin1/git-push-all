397d321c974751577df9f405e06536140a06846f
Sam <sam@future-precedent.org>
2017-10-25 02:30:04 +0100

    improved default option setting method

--- gal.js
+++ gal.js
@@ -46,36 +46,35 @@ if (command === 'config') {
 } 
 // If no command, run gal with options
 else {
+  const currentBranch = shell.exec('git branch | grep \\*', {silent:true}).stdout.slice(2);
+
   const optionDefinitions = [
     {
       name: 'message',
       alias: 'm',
       type: String,
-      defaultOption: true
+      defaultOption: true,
+      defaultValue: config.message
     },
     {
       name: 'remote',
       alias: 'r',
-      type: String
+      type: String,
+      defaultValue: config.remote
     },
     {
       name: 'branch',
       alias: 'b',
-      type: String
+      type: String,
+      defaultValue: currentBranch
     }
   ];
   
   const options = commandLineArgs(optionDefinitions, {argv});
 
-  const currentBranch = shell.exec('git branch | grep \\*', {silent:true}).stdout.slice(2);
-
-  function gal(message = config.message, remote = config.remote, branch = currentBranch) {
-    shell.exec(`git add -A &&
-      git commit -m "${message}" &&
-      git push -u ${remote} ${branch}`);
-  }
-  
-  gal(options.message, options.remote, options.branch);
+  shell.exec(`git add -A &&
+    git commit -m "${options.message}" &&
+    git push -u ${options.remote} ${options.branch}`);
 }
 
 // CommandLineArgs and CommandLineOptions auto throw errors on invalid commands or options
