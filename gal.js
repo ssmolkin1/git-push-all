2c045419fd27f51d22998cd9e69d4f79c4cdc64d
Sam <sam@future-precedent.org>
2017-10-26 03:00:09 +0100

    add commit only and auto create libsecret

--- gal.js
+++ gal.js
@@ -8,17 +8,62 @@ const commandLineCommands = require('command-line-commands');
 const configPath = './config.json';
 const config = require(configPath);
 
-const validCommands = [null, 'config'];
+const validCommands = [null, 'config', 'p'];
 const {command, argv} = commandLineCommands(validCommands);
 
-// Utility requires git to run
+// Utility requires git to work
 if (!shell.which('git')) {
-  shell.echo('Sorry, this script requires git. Please install git and try again.');
+  console.log('Sorry, this script requires git. Please install git and try again.');
   shell.exit(1);
 }
 
+if (command === 'p') {
+  shell.config.silent = true;
+  // install libsecret dev files if you don't already have them
+  // shell.exec('sudo apt install libsecret.*dev -y');    //uncomment when done testing
+
+  // checks if you have a libsecret folder in your git credentials file and mkdir if not
+  var path = '/usr/share/doc/git/contrib/credential/';
+  if (!shell.ls(path).grep('libsecret')) {
+    // checks for curl, otherwise cannot continue
+    if (!shell.which('curl')) {
+      console.log('You are not set up yet to store git credentials using libsecret. Gal can set this up for you, but curl is required to install the necessary files. Please install curl and try again.');
+      shell.exit(1);
+    }
+    shell.exec(`sudo mkdir ${path}`+'libsecret/');
+  } 
+  
+  path += 'libsecret2/';   // change to libsecret when  done testing
+  const libsecretContents = shell.ls(path);
+    
+  // checks if you have the Makefile and curl if not
+  if (libsecretContents.indexOf('Makefile') < 0) {
+    if (!shell.which('curl')) {
+      console.log('You are not set up yet to store git credentials using libsecret. Gal can set this up for you, but curl is required to install the necessary files. Please install curl and try again.');
+      shell.exit(1);
+    }
+    shell.exec(`sudo curl -o ${path}`+'Makefile https://raw.githubusercontent.com/git/git/master/contrib/credential/libsecret/Makefile');
+  }
+
+  // checks if you have the C file and curl if not
+  if (libsecretContents.indexOf('git-credential-libsecret.c') < 0) {
+    if (!shell.which('curl')) {
+      console.log('You are not set up yet to store git credentials using libsecret. Gal can set this up for you, but curl is required to install the necessary files. Please install curl and try again.');
+      shell.exit(1);
+    }
+    shell.exec(`sudo curl -o ${path}`+'git-credential-libsecret.c https://raw.githubusercontent.com/git/git/master/contrib/credential/libsecret/git-credential-libsecret.c');
+  }
+
+  // checks if made already, and make if not
+  if (libsecretContents.indexOf('git-credential-libsecret.o') < 0) {
+    shell.exec(`sudo make -C ${path}`);
+  }
+
+  // check if git is initialized
+
+}
 // Config command sets default configs
-if (command === 'config') {
+else if (command === 'config') {
   const optionDefinitions = [
     {
       name: 'message',
@@ -69,14 +114,21 @@ else {
       alias: 'b',
       type: String,
       defaultValue: currentBranch
+    },
+    {
+      name: 'commit-only',
+      alias: 'c',
+      type: Boolean,
     }
   ];
   
   const options = commandLineArgs(optionDefinitions, {argv});
 
-  shell.exec(`git add -A &&
-    git commit -m "${options.message.join(' ')}" &&
-    git push -u ${options.remote} ${options.branch}`);
+  shell.exec(`git add -A && git commit -m "${options.message.join(' ')}"`);
+  
+  if (!options[commit-only]) {
+    shell.exec(`git push -u ${options.remote} ${options.branch}`);
+  }
 }
 
 // CommandLineArgs and CommandLineOptions auto throw errors on invalid commands or options
